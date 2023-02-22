839a9f524dc06ee07e04436070bebf665e618411
TroubleRain <Troublerain1503@gmail.com>
2023-02-22 13:43:27 +0500

    do-newsletter

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+});
